e87b32a6da229fe89f0e54b5c9b916c52cdd402a
unknown <dustin.guy.jackson@gmail.com>
2020-01-02 13:29:56 -0800

    got a much more functional timer and question section running

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-// let timerEl = document.querySelector(".timer");
-// let mainEl = document.querySelector(".questions");
+let timerEl = document.querySelector(".timer");
+let mainEl = document.querySelector(".questions");
 let scoreBoard = document.querySelector("#scoreboard");
 let player = document.querySelector("#player");
 let lastName = document.querySelector("#lastName");
@@ -9,34 +9,25 @@ let submit = document.querySelector("#submit");
 let alertDiv = document.querySelector("#alertDiv");
 let playerPost = document.querySelector("#player");
 let score = 0;
-// let timer = 60;
+let timer = 60;
+let interval = setInterval(function () {
+  timer--;
+  timerEl.textContent = timer + " seconds until Borg invasion.";
+  if(timer === 0) {
+    clearInterval(interval);
+    loseSplash();
+      
+  } 
+}, 1000);
 
+function loseSplash() {
+  timerEl.textContent = " ";
 
-// function clock() {
-//   let interval = setInterval(function () {
-//     timer--;
-//     timerEl.textContent = timer + " seconds until Borg invasion.";
-  
-//     if(timer === 0) {
-//       clearInterval(interval);
-//       loseSplash();
-        
-//     }
-  
-//   }, 1000);
-// };
+  let imgEl = document.createElement("img");
 
-// function loseSplash() {
-//   timerEl.textContent = " ";
-
-//   let imgEl = document.createElement("img");
-
-//   imgEl.setAttribute("src", "assets/images/borg.gif");
-//   mainEl.replaceWith(imgEl);
-// };
-
-// clock();
-
+  imgEl.setAttribute("src", "assets/images/borg.gif");
+  mainEl.replaceWith(imgEl);
+};
 
 const questions = [
     {
@@ -67,6 +58,31 @@ let a1 = document.getElementById("a1text");
 let a2 = document.getElementById("a2text");
 let a3 = document.getElementById("a3text");
 let a4 = document.getElementById("a4text");
+
+let i = 0;
+title.innerHTML = questions[i].title;
+a1.innerHTML = questions[i].choices[0];
+a2.innerHTML = questions[i].choices[1];
+a3.innerHTML = questions[i].choices[2];
+a4.innerHTML = questions[i].choices[3];
+
+document.querySelector(".answers").addEventListener("click", nextQuestion);
+function nextQuestion(e) {
+  e.preventDefault()
+  if (i < questions.length - 1) {
+    i++;
+    title.innerHTML = questions[i].title;
+    a1.innerHTML = questions[i].choices[0];
+    a2.innerHTML = questions[i].choices[1];
+    a3.innerHTML = questions[i].choices[2];
+    a4.innerHTML = questions[i].choices[3];    
+  } else {
+    clearInterval(interval);
+    // window.location.href = "results.html"; 
+  }
+  console.log(i); 
+}
+
 
 
 

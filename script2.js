7379d3e37f3f650e5887809a3416f2668164ce22
unknown <dustin.guy.jackson@gmail.com>
2020-01-05 22:30:42 -0800

    Ive given up on pushing a persistant locally stored array onto a list. If the storage to ok anything but strings, this would be a lot less of a headache. I'm happy with the rest, though

--- script2.js
+++ script2.js
@@ -5,41 +5,29 @@ let nameBut = document.querySelector("#name");
 let submit = document.querySelector("#submit");
 let alertDiv = document.querySelector("#alertDiv");
 let playerPost = document.querySelector("#player");
-let ensigns = [];
-ensigns.push(JSON.parse(localStorage.getItem('ensigns')));
-localStorage.setItem('ensigns', JSON.stringify(ensigns));
 let score = localStorage.getItem("score");
 let timer = 60;
-
-function SaveDataToLocalStorage(data)
-{
-    var ensigns = [];
-   
-    ensigns = JSON.parse(localStorage.getItem('ensigns'));
-    
-    ensigns.push(data);
-
-    localStorage.setItem('ensigns', JSON.stringify(ensigns));
-}
 
 submit.addEventListener("click", function(e) {
     e.preventDefault();
     let name = nameBut.value;
-    let user = {
-      name: name,
-      score: score
-      };
-      SaveDataToLocalStorage(user);
-
-    $(submit).replaceWith("<button><a href='index.html'>Launch New Mission?</a></button>")
-    console.log(name)
+    let user = name + "'s score: " + score;
+    let li = document.createElement("li");
+    li.textContent = user;
+    scoreBoard.appendChild(li);
+    
+      
+  
+      
+    // $(submit).replaceWith("<button><a href='index.html'>Launch New Mission?</a></button>")
     console.log(user)
     
-    
   });
-  
 
-  $(scoreBoard).append("<li>test</li>");
+     
+    
+
+  
 
 
 
